b0556145532e32f823ee006c3b2e2d371d1fff8b
José Victor <zevicctor2@gmail.com>
2024-04-21 18:10:52 +0000

    working fake times in jest

---
+++ modulo03-conceitos-tajs/aula04-fake-times/jest.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+const config = {
+  cearMocks: true,
+  cverageProvider: "v8",
+}
+
+export default config;
---
+++ modulo03-conceitos-tajs/aula04-fake-times/src/index.js
@@ -0,0 +1,27 @@
+import Task from "./tasks.js";
+
+const oneSecond = 1000
+const runInASec = new Date(Date.now() + oneSecond)
+const runInTwSecs = new Date(Date.now() + oneSecond * 2)
+const runInThreeSecs = new Date(Date.now() + oneSecond * 3)
+
+const task = new Task()
+task.save({
+  name: 'Task 1',
+  dueAt: runInASec,
+  fn: () => console.log('task1 executed')
+})
+
+task.save({
+  name: 'Task 2',
+  dueAt: runInTwSecs,
+  fn: () => console.log('task2 executed')
+})
+
+task.save({
+  name: 'Task 3',
+  dueAt: runInThreeSecs,
+  fn: () => console.log('task3 executed')
+})
+
+task.run(oneSecond)
